e7aecf0c6f497e00ca4b466af3957d3bad9b8eeb
NashStrife <jac.sebastien@gmail.com>
2016-07-06 13:56:53 +0200

    edit routes and controller

--- server/api/crm/controller.js
+++ server/api/crm/controller.js
@@ -2,7 +2,7 @@ let logger = require(`${process.cwd()}/server/utils/logger`);
 // get an instance of the model of our db
 let model = require('./model');
 
-exports.get = function(req, res, next) {
+exports.getCustomer = function(req, res, next) {
     logger.log("controller get")
     // find() function of the model thanks to Mongoose [cf : http://mongoosejs.com/docs/queries.html]
     model.Customer.find()
@@ -14,7 +14,7 @@ exports.get = function(req, res, next) {
 };
 
 
-exports.post = function(req, res, next) {
+exports.postCustomer = function(req, res, next) {
     logger.log("controller post");
     
     // we create a new model with the data to add to the db
@@ -34,11 +34,11 @@ exports.post = function(req, res, next) {
 };
 
 
-exports.update = function(req, res, next) {
+exports.updateCustomer = function(req, res, next) {
     logger.log("controller Update")
     
     // update the data corresponding to the id with the new one in the request
-    model.findByIdAndUpdate(req.params.id, req.body, 
+    model.Customer.findByIdAndUpdate(req.params.id, req.body, 
         function(err, doc) {
             let message = {
                 message: 'Document upated'
@@ -56,7 +56,7 @@ exports.deleteById = function(req, res, next) {
     logger.log("controller deleteById");
     
     // get id from the parameters sent in the url
-    model.findByIdAndRemove(req.params.id,
+    model.Customer.findByIdAndRemove(req.params.id,
         function(err, doc) {
             let message = {
                 message: 'Document removed'
@@ -75,7 +75,7 @@ exports.dynamicSearch = function(req, res, next) {
 
     let query = req.query;
     console.log(req.query);
-    model.find(query)
+    model.Customer.find(query)
     .then(function(docs){
         // the result is not empty we have a corresponding result
         if(docs.length) {
@@ -86,4 +86,33 @@ exports.dynamicSearch = function(req, res, next) {
             res.json({message:`No result for the query ${JSON.stringify(query)}`});
         }
     });
+};
+
+exports.getAdmin = function(req, res, next) {
+    logger.log("controller get")
+    // find() function of the model thanks to Mongoose [cf : http://mongoosejs.com/docs/queries.html]
+    model.find()
+    // once the query is done, do the following action thanks to a "promise"
+    .then(function(docs){
+        logger.log(docs);
+        res.json(docs);
+    });
+};
+
+exports.updateAdmin = function(req, res, next) {
+    logger.log("controller Update")
+    
+    // update the data corresponding to the id with the new one in the request
+    model.findByIdAndUpdate(req.params.id, req.body, 
+        function(err, doc) {
+            let message = {
+                message: 'Document upated'
+            };
+            if (err) {
+                message.message = err.message;
+            }
+            logger.log(message);
+            res.json(message);
+        }
+    );
 };
--- server/api/crm/routes.js
+++ server/api/crm/routes.js
@@ -3,18 +3,19 @@ let controller = require('./controller');
 
 
 router.route('/')
-.get(controller.get)
-.post(controller.post);
+.get(controller.getCustomer)
+.post(controller.postCustomer);
 
-// router.route('/admin')
-// .put(controller.adminUpdate);
+router.route('/admin')
+.get(controller.getAdmin)
+.put(controller.updateAdmin);
 
 // we want dynamic params so we don't use /:search but the old school /search
 router.route('/search')
 .get(controller.dynamicSearch);
 
 router.route('/:id')
-.put(controller.update)
+.put(controller.updateCustomer)
 .delete(controller.deleteById);
 
 
