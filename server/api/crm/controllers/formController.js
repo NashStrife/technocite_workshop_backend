7f98a422952d2e35657e0746baf3c97e1d0e6b7e
NashStrife <jac.sebastien@gmail.com>
2016-08-05 12:20:54 +0200

    cleaning code

--- server/api/crm/controllers/formController.js
+++ server/api/crm/controllers/formController.js
@@ -10,12 +10,15 @@ const dest_folder = 'public/images/uploads/';
 
 var storage = multer.diskStorage({ //multers disk storage settings
     destination: function (req, file, cb) {
-        logger.log("--- FORM CONTROLLER : destination");
-        cb(null, dest_folder);
+        logger.log("--- FORM CONTROLLER : storage.destination");
+        if(req.body.folder)
+            cb(null, req.body.folder);
+        else
+            cb(null, dest_folder);
     },
     filename: function (req, file, cb) {
-        logger.log("--- FORM CONTROLLER : filename");
-        console.log(req.body);
+        logger.log("--- FORM CONTROLLER : storage.filename");
+        // console.log(req.body);
         // var datetimestamp = Date.now();
         // define the file name [name from the form-timestamp.extension]
         // cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]); 
@@ -34,8 +37,8 @@ exports.uploadImage = function(req, res, next) {
     logger.log("--- FORM CONTROLLER : uploadImage");
     upload(req, res, function(err){
         // req.file is the file named `file` [corresponding to the .single() method]
-        logger.log(req.body);
-        logger.log(req.file);
+        // logger.log(req.body);
+        // logger.log(req.file);
         if(err){
                 res.json({error_code:1,err_desc:err});
                 logger.log(err);
@@ -44,6 +47,3 @@ exports.uploadImage = function(req, res, next) {
             res.json({error_code:0,err_desc:null});
     });
 };
-
-// manque :
-// ajout db
--- server/utils/uploadImage.js
+++ server/utils/uploadImage.js
@@ -1,35 +1,29 @@
-let uploadImage = {
-    storage: function() {
-        multer.diskStorage({ //multers disk storage settings
-            destination: function (req, file, cb) {
-                cb(null, './uploads/');
-            },
-            filename: function (req, file, cb) {
-                var datetimestamp = Date.now();
-                cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]);
-            }
-        });
+let logger = require(`${process.cwd()}/server/utils/logger`);
+let multer = require('multer');
+const dest_folder = 'public/images/uploads/';
+
+var storage = multer.diskStorage({ //multers disk storage settings
+    destination: function (req, file, cb) {
+        logger.log("--- UPLOAD IMAGE : storage.destination");
+        cb(null, dest_folder);
     },
-    upload: function() {
+    filename: function (req, file, cb) {
+        logger.log("--- UPLOAD IMAGE : storage.filename");
+        console.log(req.body);
+        // var datetimestamp = Date.now();
+        // define the file name [name from the form-timestamp.extension]
+        // cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]); 
+        cb(null, req.body.filename + '.jpg');
+    }
+});
+
+let uploadImage = {
+    upload: function(req, res, cb) {
+        logger.log("--- UPLOAD IMAGE : upload");
         multer({ //multer settings
                 storage: storage
-            }).single('file');
+            }).single('file'); // req.file is the file named `file` 
     }
 }
 
 module.exports = uploadImage;
-
-
-// var storage = multer.diskStorage({ //multers disk storage settings
-//     destination: function (req, file, cb) {
-//         cb(null, './uploads/');
-//     },
-//     filename: function (req, file, cb) {
-//         var datetimestamp = Date.now();
-//         cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]);
-//     }
-// });
-
-// var upload = multer({ //multer settings
-//                 storage: storage
-//             }).single('file');
